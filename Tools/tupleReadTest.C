ca56459fa7e86d2a091272e884d3930cb6777e1a
Nathaniel <pastika@cern.ch>
2015-01-06 13:48:54 -0600

    Tuple reader added in complete form

--- Tools/tupleReadTest.C
+++ Tools/tupleReadTest.C
@@ -5,17 +5,29 @@
 #include "TChain.h"
 
 #include <iostream>
+#include <cstdio>
+#include <string>
 
 int main()
 {
-    TFile *f = new TFile("/eos/uscms/store/user/pastika/DYJetsToLL_M-50_13TeV-madgraph-pythia8/PHYS14_PU20bx25_PHYS14_25_V1-FLAT/141227_223539/0000/stopFlatNtuples_4.root");
-    
-    TTree *t = (TTree*)f->Get("stopTreeMaker/AUX");
+    char nBase[] = "/eos/uscms/store/user/pastika/DYJetsToLL_M-50_13TeV-madgraph-pythia8/PHYS14_PU20bx25_PHYS14_25_V1-FLAT/141227_223539/0000/stopFlatNtuples_%d.root";
 
-    NTupleReader tr(t);
+    char fName[256];
+
+    TChain *f = new TChain("stopTreeMaker/AUX");
+
+    for(int i = 0; i < 10; ++i)
+    {
+        sprintf(fName, nBase, i);
+        f->Add(fName);
+    }
+    
+    //TTree *t = (TTree*)f->Get("stopTreeMaker/AUX");
+
+    NTupleReader tr(f);
 
     while(tr.getNextEvent())
     {
-        //std::cout << tr.getNEntries() << "\t" << tr.run << "\t" << tr.lumi << "\t" << tr.event << std::endl;
+//        std::cout << tr.getNEntries() << "\t" << tr.run << "\t" << tr.lumi << "\t" << tr.event << "\t" << tr.genDecayLVec->size() << "\t" <<  tr.mht << std::endl;
     }
 }
